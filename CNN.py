1cf35914946294695b7341d85459b019ac9bca2a
= <=>
2020-05-23 21:17:27 +0700

    add CNN.py

--- CNN.py
+++ CNN.py
@@ -1,8 +1,10 @@
 import numpy as np 
 import keras
-import random
 import cv2
+import random
 from keras.layers import Conv2D, MaxPooling2D, Activation, Flatten, Dense
+
+np.random.seed(42)
 
 C = 2
 N = 8
@@ -29,7 +31,7 @@ for i in ListTrain:
         path = path + 'Ngoclinh\\' + 'S- ' + str(i - int(N / 2)) + '.jpg'
         LabelTrain.append([0, 1])
     img = cv2.imread(path, 1)
-    img = cv2.resize(img, (96, 96))
+    img = cv2.resize(img, (156, 156))
     TrainSet.append(img)
 
 LabelTrain = np.array(LabelTrain)
@@ -50,7 +52,7 @@ for i in ListTest:
         path = path + 'Ngoclinh\\' + 'S- 1 (' + str(i - int(N / 8)) + ').jpg'
         LabelTest.append([0, 1])
     img = cv2.imread(path, 1)
-    img = cv2.resize(img, (96, 96))
+    img = cv2.resize(img, (156, 156))
     TestSet.append(img)
 
 LabelTest = np.array(LabelTest)
@@ -62,38 +64,38 @@ print('Number element of Test Set: ', len(TestSet))
 model = keras.Sequential()
 
 # First Conv
-model.add(Conv2D(8, 3, padding= 'valid', input_shape= TrainSet.shape[1:])) # Add Conv layer
+model.add(Conv2D(16, 3, padding= 'valid', input_shape= TrainSet.shape[1:])) # Add Conv layer
 model.add(Activation('relu')) # Add Activation Function
-model.add(Conv2D(8, 3, padding= 'valid')) # Add Conv layer
-model.add(Activation('relu')) #Add Activation Function
+model.add(Conv2D(16, 3, padding= 'valid')) # Add Conv layer
+model.add(Activation('relu')) # Add Activation Function
 model.add(MaxPooling2D(pool_size= (2, 2))) # Add Pooling layer
 
 # Second Conv
-model.add(Conv2D(16, 3, padding= 'valid')) # Add Conv layer
+model.add(Conv2D(32, 3, padding= 'valid')) # Add Conv layer
 model.add(Activation('relu')) # Add Activation Function
-model.add(Conv2D(16, 3, padding= 'valid')) # Add Conv layer
+model.add(Conv2D(32, 3, padding= 'valid')) # Add Conv layer
 model.add(Activation('relu')) # Add Activation Function
 model.add(MaxPooling2D(pool_size= (2, 2))) # Add Pooling layer
 
 # Third Conv
-model.add(Conv2D(32, 3, padding= 'valid')) # Add Conv layer
+model.add(Conv2D(64, 3, padding= 'valid')) # Add Conv layer
 model.add(Activation('relu')) # Add Activation Function
-model.add(Conv2D(32, 3, padding= 'valid')) # Add Conv layer
+model.add(Conv2D(64, 3, padding= 'valid')) # Add Conv layer
 model.add(Activation('relu')) # Add Activation Function
 model.add(MaxPooling2D(pool_size= (2, 2))) # Add Pooling layer
 
 # Full Conected
 model.add(Flatten()) # Flatten
-model.add(Dense(32))
+model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dense(2))
 model.add(Activation('softmax'))
 
-#Train Model
-model.compile(loss= 'categorical_crossentropy', optimizer= keras.optimizers.SGD(learning_rate= 0.01), metrics= ['accuracy'])
+# Train Model
+model.compile(loss= 'categorical_crossentropy', optimizer= keras.optimizers.Adam(learning_rate= 0.01), metrics= ['accuracy'])
 model.fit(TrainSet, LabelTrain, batch_size= N, epochs= epochs, validation_data= (TestSet, LabelTest))
 
-#Get Score
+# Get Score
 score = model.evaluate(TestSet, LabelTest)
 
 print('Test Loss: ', score[0])
